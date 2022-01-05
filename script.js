cd58d8374ec7a982360333a2c2fac4aa77fe9fad
luiscarriel <vitorluiiscarriel@gmail.com>
2022-01-05 15:02:50 -0500

    update completed

---
+++ script.js
@@ -0,0 +1,17 @@
+function clicar (){
+    
+    const cep = document.getElementById("cep").value
+
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then(response => response.json())
+    .then(endereco => {
+        console.log(endereco.cep)
+        console.log(endereco.logradouro)
+        console.log(endereco.bairro)
+        console.log(endereco.localidade)
+        console.log(endereco.ddd)
+    })
+    .catch(error => {
+        console.log("deu merda")
+    }) 
+}
