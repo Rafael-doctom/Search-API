74b2c0be3c9250564982e269756a5d5ee1dce6a0
luiscarriel <vitorluiiscarriel@gmail.com>
2022-03-25 15:08:42 -0500

    feat: Correction of result in innerHTML

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function clicar (){
+    const cep = document.getElementById("cep").value
+    const px = document.getElementById("contador")
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then(response => response.json())
+    .then(endereco => {
+        px.innerHTML = `<div>
+        CEP: ${endereco.cep} <br></br> 
+        Logradouro: ${endereco.logradouro} <br></br>
+        Bairro: ${endereco.bairro} <br></br>
+        Localidade: ${endereco.localidade} <br></br>
+        DDD: ${endereco.ddd}
+        </div>`
+        
+        
+        console.log("CEP", endereco.cep)
+        console.log("Logradouro", endereco.logradouro)
+        console.log("Bairro", endereco.bairro)
+        console.log("Localidade", endereco.localidade)
+        console.log("DDD", endereco.ddd)
+
+    })
+    .catch(error => {
+        console.log("erro")
+    }) 
+}
